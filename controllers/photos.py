cdccc0046c434d98176ffa41f8db3c7f04ad2d2b
Julius Ware <jcware8@gmail.com>
2019-07-01 15:22:07 -0400

    Import template file, add new folio view

--- controllers/photos.py
+++ controllers/photos.py
@@ -18,3 +18,17 @@ def get_photos():
         return jsonify({'Exception': str(e)})
 
     return render_template('pages/photos.html', photo_urls=photo_urls)
+
+
+@photos_blueprint.route('/folio', methods=['GET'])
+def get_folio_view():
+
+    try:
+        flickr.set_keys(api_key='099497565f524eb996ba54810de4a555', api_secret='0409aaa514e77497')
+        photos = flickr.Person.findByUserName("warejc").getPublicPhotos()
+        photo_urls = [photo.getPhotoFile(size_label="Medium") for photo in photos]
+    except Exception as e:
+        return jsonify({'Exception': str(e)})
+
+    return render_template('pages/portfolio.html', photo_urls=photo_urls)
+
